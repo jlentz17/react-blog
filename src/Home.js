610ebcc3d43cb2f90cd2211ef5ef51eef492e85a
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-10-30 17:17:55 -0700

    practiced with state and forgot to make an element to print to the screen

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Home() {
-    return (
-        <div className="home">
-            <h2>Homepage</h2>
-        </div>
-    )
+const [counter, setCounter] = useState(0);
+  const handleClick = () => {
+    console.log("Fuuuuck");
+  };
+//   const handleClickAgain = () => (
+//     setCounter()
+//   );
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <p>{counter}</p>
+      <button onClick={() => setCounter(counter+1)}>Click Me again</button>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
